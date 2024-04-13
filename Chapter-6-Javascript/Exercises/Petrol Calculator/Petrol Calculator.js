5a46dd4a733f690e63e8cdea74a13946ce791d25
ASFA-TARIQ <asfatariq123@gmail.com>
2024-04-13 16:42:18 +0400

    version 1

--- Chapter-6-Javascript/Exercises/Petrol Calculator/Petrol Calculator.js
+++ Chapter-6-Javascript/Exercises/Petrol Calculator/Petrol Calculator.js
@@ -1,6 +1,6 @@
 function CalculateTotal() {
-    var CostPerLiter = parseFloat(document.getElementById('Cost-Per-Liter').value);
-    var LitersPurchased = parseFloat(document.getElementById('Liters-Purchased').value);
-    var TotalCost = CostPerLiter * LitersPurchased;
-    document.getElementById('Total-Cost').innerText = "Total Cost: AED" + TotalCost.toFixed(2);
+    var CostPerLiter = parseFloat(document.getElementById('Cost-Per-Liter').value); //Getting user data by id
+    var LitersPurchased = parseFloat(document.getElementById('Liters-Purchased').value);  //Getting user data by id
+    var TotalCost = CostPerLiter * LitersPurchased; //Calculating total of CostPerLiter LitersPurchased
+    document.getElementById('Total-Cost').innerText = "Total Cost: AED" + TotalCost.toFixed(2); //displaying totql cost
 }
